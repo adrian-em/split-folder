566e5ea36b230edba38887fd5275cdfac8d31ca0
aesptux <aespinosamoreno@gmail.com>
2013-03-23 12:07:14 +0100

    PEP8

--- splitfolder/splitfolder.py
+++ splitfolder/splitfolder.py
@@ -1,13 +1,16 @@
+"""
+Split a big folder into X smaller folders
+"""
 import os
 from docopt import docopt
 import shutil
 import logging
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'aesptux'
 logging.basicConfig(level=logging.DEBUG)
-log = logging.getLogger(__name__)
-args = """
+LOG = logging.getLogger(__name__)
+ARGS = """
 Usage:
     splitfolder.py <folder> <newfoldername> <elements>
 
@@ -40,20 +43,19 @@ def folder_creator(newfoldername, counter):
     """
     Handler to create folder
     """
-    log.info("Entering. creator")
+    LOG.info("Entering. creator")
     counter = int(counter)
     counter += 1
-    #print counter
-    log.info("Value of counter is {0}".format(counter))
+    LOG.info("Value of counter is {0}".format(counter))
     try:
-        log.info("Enter try.")
+        LOG.info("Enter try.")
         foldername = "../{0}_{1}".format(newfoldername, str(counter))
         #print foldername
-        log.info("Foldername is {0}".format(foldername))
+        LOG.info("Foldername is {0}".format(foldername))
         os.mkdir('{0}'.format(foldername))
         return {'foldername': foldername, 'counter': counter}
     except OSError:
-        log.info("OSError raised")
+        LOG.info("OSError raised")
         return folder_creator(newfoldername, counter)
 
 
@@ -62,7 +64,7 @@ def main(folder, newfoldername, elements):
     Main
     """
     check_folder(folder)
-    log.info("Entering. main")
+    LOG.info("Entering. main")
     elements = int(elements)
     counter = 0
     os.chdir(folder)
@@ -73,17 +75,14 @@ def main(folder, newfoldername, elements):
             returned_dict = folder_creator(newfoldername, counter)
             counter = returned_dict['counter']
             foldername = returned_dict['foldername']
-            log.info("Value of counterMAIN is {0}".format(counter))
-            log.info("FoldernameMAIN is {0}".format(foldername))
-        #print counter
-        #print foldername
-        #print returned_dict
+            LOG.info("Value of counterMAIN is {0}".format(counter))
+            LOG.info("FoldernameMAIN is {0}".format(foldername))
 
         print "Moving {0}".format(elem)
         shutil.move(elem, foldername)
 
 if __name__ == '__main__':
-    arguments = docopt(args, version=__version__)
-    main(folder=arguments['<folder>'],
-        newfoldername=arguments['<newfoldername>'],
-        elements=arguments['<elements>'])
+    ARGUMENTS = docopt(ARGS, version=__version__)
+    main(folder=ARGUMENTS['<folder>'],
+        newfoldername=ARGUMENTS['<newfoldername>'],
+        elements=ARGUMENTS['<elements>'])
